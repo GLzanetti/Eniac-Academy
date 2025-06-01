cff261f24cb23e3971470aa3caca3c58ae0432d3
Gabriel <gabrielzanettigo@gmail.com>
2025-05-31 21:01:27 -0300

    Adicionando imagens do background, horários, logo e finalizando a tela de perfil

--- app/src/main/java/com/example/eniacacademy/controller/login.java
+++ app/src/main/java/com/example/eniacacademy/controller/login.java
@@ -11,7 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.eniacacademy.R;
 
-public class Login extends AppCompatActivity {
+public class login extends AppCompatActivity {
 
     EditText txtEmail;
     EditText txtSenha;
---
+++ app/src/main/java/com/example/eniacacademy/model/CriaBanco.java
@@ -0,0 +1,5 @@
+package com.example.eniacacademy.model;
+
+public class CriaBanco {
+
+}
