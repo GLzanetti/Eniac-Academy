dde34eacc741a073095eb0f83fae95daa3bcb07e
Felipe <219332024@eniac.edu.br>
2025-06-11 23:05:35 -0300

    adicionei as validacoes do cadastro2

--- app/src/main/java/com/example/eniacacademy/controller/cadastro2.java
+++ app/src/main/java/com/example/eniacacademy/controller/cadastro2.java
@@ -2,9 +2,11 @@ package com.example.eniacacademy.controller;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +18,7 @@ import com.example.eniacacademy.R;
 
 public class cadastro2 extends AppCompatActivity {
 
-    EditText txtEmail, txtSenha, txtConfirmasenha;
+    EditText txtEmail, txtSenha, txtConfirmaSenha;
     Button btCadastro2, btVoltarCadastro;
 
     @Override
@@ -32,7 +34,7 @@ public class cadastro2 extends AppCompatActivity {
 
         btCadastro2 = findViewById(R.id.btCadastro2);
         btVoltarCadastro = findViewById(R.id.btVoltarCadastro);
-        txtConfirmasenha = findViewById(R.id.txtConfirmasenha);
+        txtConfirmaSenha = findViewById(R.id.txtConfirmasenha);
         txtEmail = findViewById(R.id.txtEmail);
         txtSenha = findViewById(R.id.txtSenha);
 
@@ -47,9 +49,36 @@ public class cadastro2 extends AppCompatActivity {
         btCadastro2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(cadastro2.this, mainActivity.class);
-                startActivity(intent);
+                if (validarCadastro2()){
+                    Intent intent = new Intent(cadastro2.this, mainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
+    }
+
+    public boolean validarCadastro2(){
+        String confirmaSenha = txtConfirmaSenha.getText().toString();
+        String email = txtEmail.getText().toString();
+        String senha = txtSenha.getText().toString();
+        String msg = "";
+        boolean retorno = true;
+
+        if(email.isEmpty() || !email.contains("@gmail.com")) {
+            msg = "Preencha o email corretamente";
+            Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+            retorno = false;
+
+        } else if(senha.isEmpty() || senha.length() < 6 ) {
+            msg = "A senha deve conter 6 caracteres";
+            Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+            retorno = false;
+        } else if(confirmaSenha != senha){
+            msg = "A senha deve ser igual ao campo de cima";
+            Toast.makeText(this,msg, Toast.LENGTH_LONG).show();
+            retorno = false;
+        }
+
+        return retorno;
     }
 }
